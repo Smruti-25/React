1b8dea0a4a6fd244c07348d7fd40dbb20ec1ad09
sk2597 <smruti.karade25@gmail.com>
2024-02-27 22:37:45 +0530

    since import is not normal javascript we hae to mention type = module in the script tag

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
-/* <div id = "parent">
-  <div id = "child">
-    <h1>i am h1 tag</h1>
-    <h2>i am h2 tag</h2>
-  </div>
-</div> */
+import React from "react";
+import ReactDOM from "react-dom";
+
 
 const parent = React.createElement(
   "div", 

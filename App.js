145da9f689b26bb6dc1b7a96b2ec2df6aa1b0605
sk2597 <smruti.karade25@gmail.com>
2024-05-17 14:31:26 +0530

    added header component and css to style it

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const someElement = <h2>some element</h2>
-const Title = () => <h1>Page Title</h1>
-const HeaderComponent = () => (
-  <div id="container">
-    <Title />
-    {someElement}
-    <h1>Header Component</h1>
-  </div>
-);
+//todo add cart logo
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://i.pinimg.com/originals/44/10/8a/44108ac1e31ee550c8276f7c7035a544.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header/>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeaderComponent />);
+root.render(<AppLayout />);

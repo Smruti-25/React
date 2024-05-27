99dbbdc1db7da485c52c773139347f8af8f2baf6
sk2597 <smruti.karade25@gmail.com>
2024-05-27 20:18:32 +0530

    added error component and errorElement to the router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
 import ContactUs from "./components/ContactUs";
+import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 //todo add cart logo
@@ -20,16 +21,19 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <Error />,
   },
 
   {
     path: "/about",
     element: <About />,
+    errorElement: <Error />,
   },
 
   {
     path: "/contactus",
     element: <ContactUs />,
+    errorElement: <Error />,
   },
 ]);
 

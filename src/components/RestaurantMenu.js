50ff19eac1a518320754d69031ea0045faf8fd56
sk2597 <smruti.karade25@gmail.com>
2024-05-28 10:45:12 +0530

    restaurant info state var is enough for both menu and resData

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -6,31 +6,45 @@ const RestaurantMenu = () => {
     fetchMenu();
   }, []);
 
-  const [restaurantData, setRestaurantData] = useState([]);
-  const [restaurantMenu, setRestaurantMenu] = useState([]);
+  const [restaurantInfo, setRestaurantInfo] = useState(null);
 
-  const fetchMenu = async () => {
+  const fetchMenu = async() => {
     const jsonData = await fetch(
       "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.11610&lng=79.07060&restaurantId=553826&catalog_qa=undefined&submitAction=ENTER"
     );
     const json = await jsonData.json();
 
     console.log(json);
-    const restaurantData = json?.data?.cards[2]?.card?.card?.info;
-    const restaurantMenu = json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards;
-
-    console.log(restaurantData, restaurantMenu);
-    setRestaurantData(restaurantData);
-    setRestaurantMenu(restaurantMenu);
+    // const restaurantData = json?.data?.cards[2]?.card?.card?.info;
+    // const restaurantMenu = json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards;
+    setRestaurantInfo(json.data);
   };
 
-  return restaurantData.length === 0? ( <Shimmer/> ) : (
+  if(restaurantInfo === null) return <Shimmer/>
+
+  const { name, costForTwoMessage, avgRating, cuisines } =
+    restaurantInfo?.cards[2]?.card?.card?.info;
+  
+  const { itemCards } =
+    restaurantInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+      ?.card?.itemCards;
+
+  return (
     <div className="menu">
-      <h1>
-        I am the restaurant menu Page
-      </h1>
+      <div>
+        <h1>{name}</h1>
+        <h2>
+          {cuisines.join(", ")} - {costForTwoMessage}
+        </h2>
+        <h2>{avgRating}</h2>
+
+        <h2>Menu</h2>
+        <ul>
+          <li>{itemCards}</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
 export default RestaurantMenu;

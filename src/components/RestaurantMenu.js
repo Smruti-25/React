583fd71d9d4810495fa84ff0bbb2c2071c31cde7
sk2597 <smruti.karade25@gmail.com>
2024-05-28 09:51:04 +0530

    added state variables for resturant data and menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,15 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const RestaurantMenu = () => {
   useEffect(() => {
     fetchMenu();
   }, []);
 
+  const [restaurantData, setRestaurantData] = useState([]);
+  const [restaurantMenu, setRestaurantMenu] = useState([]);
+
   const fetchMenu = async () => {
-    const jsonData = await fetch("");
+    const jsonData = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.11610&lng=79.07060&restaurantId=553826&catalog_qa=undefined&submitAction=ENTER"
+    );
     const json = await jsonData.json();
 
     console.log(json);
+    const restaurantData = json?.data?.cards[2]?.card?.card?.info;
+    const restaurantMenu = json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards;
+
+    console.log(restaurantData, restaurantMenu);
+    setRestaurantData(restaurantData);
+    setRestaurantMenu(restaurantMenu);
   };
 
   return (

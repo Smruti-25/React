1fca7f0f445fc7815e662cf7ae22a62b0c5f0f1c
sk2597 <smruti.karade25@gmail.com>
2024-05-29 12:51:45 +0530

    imported react in the class based component, exported UserClass component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,3 +1,14 @@
+import React from "react";
 class UserClass extends React.Component {
-
+  render(){
+    return (
+      <div className="user-card">
+        <h1>Smruti Karade</h1>
+        <h2>Nagpur</h2>
+        <h2>Contact Info</h2>
+      </div>
+    );
+  }
 }
+
+export default UserClass;

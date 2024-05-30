1fdf1507f043fddee9b946f01fe0697156d0383c
sk2597 <smruti.karade25@gmail.com>
2024-05-30 14:02:12 +0530

    lazy loading about us component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import AboutUs from "./components/About";
 import ContactUs from "./components/ContactUs";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const Grocery = lazy(()=>import("./components/Grocery"));
+const AboutUs = lazy(() => import("./components/About"));
 
 //todo add cart logo
 const AppLayout = () => {
@@ -31,7 +31,11 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <AboutUs />,
+        element: (
+          <Suspense fallback={<h1>Loading about us...</h1>}>
+            <AboutUs />
+          </Suspense>
+        ),
       },
 
       {

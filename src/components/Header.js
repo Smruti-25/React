e35a420c60570b6e1c4bdd0f94f9c23923331c2d
sk2597 <smruti.karade25@gmail.com>
2024-05-30 13:33:16 +0530

    linked path to grocery on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,6 +23,9 @@ const Header = () => {
           <li>
             <Link to={"/contactus"}>Contact Us</Link>
           </li>
+          <li>
+            <Link to={"/grocery"}>Grocery</Link>
+          </li>
           <li>Cart</li>
           <button
             className="login"

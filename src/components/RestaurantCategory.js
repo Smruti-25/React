6631f80d03beae2d6283b618e1f48d9b9bbbc39b
sk2597 <smruti.karade25@gmail.com>
2024-06-04 11:47:52 +0530

    adding tailwind css for accordion width is 6/12

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,9 +1,13 @@
-const RestaurantCategory = () => {
+const RestaurantCategory = ({data}) => {
+  console.log({data});
   return (
-    <div>
-      Restaurant category
+    <div className="w-6/12 bg-gray-100 shadow-lg p-4 mx-auto my-4 flex justify-between">
+      <span className="font-bold text-lg">
+        {data.title} ({data.itemCards.length})
+      </span>
+      <span>⬇️</span>
     </div>
-  )
+  );
 }
 
 export default RestaurantCategory;

ff39d6a695b517d31924f7abb0ad4c675d2076b1
sk2597 <smruti.karade25@gmail.com>
2024-06-04 13:39:41 +0530

    itemList component

--- src/components/itemList.js
+++ src/components/itemList.js
@@ -1,15 +1,21 @@
-const ItemList = ({items}) => {
-  console.log({items});
+const ItemList = ({ items }) => {
+  console.log({ items });
   return (
     <div>
-      {items.map((i) => (
-        <div key={i?.card?.info?.id}>
-          <span>{i?.card?.info?.name}</span>
-          <span>{i?.card?.info?.price}</span>
+      {items.map((item) => (
+        <div
+          key={item?.card?.info?.id}
+          className="p-2 m-2 border-b-2 border-gray-200 "
+        >
+          <div>
+            <span>{item?.card?.info?.name}</span>
+            <span>{item?.card?.info?.price}</span>
+          </div>
+          <p className="text-xs">{item.card.info.description}</p>
         </div>
       ))}
     </div>
   );
-}
+};
 
 export default ItemList;

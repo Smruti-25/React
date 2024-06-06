3a52e1e4f6578a2b5ddee763c347e48bddff6d0f
sk2597 <smruti.karade25@gmail.com>
2024-06-06 15:42:37 +0530

    dummy authentication logic in useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -9,6 +9,18 @@ import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const Grocery = lazy(()=>import("./components/Grocery"));
 const AboutUs = lazy(() => import("./components/About"));
+
+const [userName, setUserName] = useState();
+
+useEffect(() => {
+  {
+    //some authentication logic
+    const data = {
+      name: "Smruti",
+    };
+  }
+  setUserName(data.name);
+}, []);
 
 //todo add cart logo
 const AppLayout = () => {

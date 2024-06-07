7c132d1573f9856c6138c9a134669337ac63995b
sk2597 <smruti.karade25@gmail.com>
2024-06-07 19:28:10 +0530

    displaying cart items handling clearCart dispatch

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,27 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import MenuItemList from "./MenuItemList";
+import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
-
   const cartItems = useSelector((store) => store.cart.items);
+  const dispatch = useDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  }
 
-  return(
+  return (
     <div className="text-center m-4 p-4">
       <h1 className="text-2xl font-bold">Cart</h1>
+      <button className="p-2 m-2 bg-black text-white rounded-lg"
+        onClick={handleClearCart}
+      >
+        Clear Cart
+      </button>
+      <div className="w-6/12 m-auto">
+        <MenuItemList items={cartItems} />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Cart;

3d5e123156d582cb560cf33658f967cc5739a077
sk2597 <smruti.karade25@gmail.com>
2024-06-07 19:16:50 +0530

    subscribing to the store'

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,9 @@
+import { useSelector } from "react-redux";
+
 const Cart = () => {
+
+  const cartItems = useSelector((store) => store.cart.items);
+
   return(
     <div className="text-center m-4 p-4">
       <h1 className="text-2xl font-bold">Cart</h1>

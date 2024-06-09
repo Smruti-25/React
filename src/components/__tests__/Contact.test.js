e4f1eeb8efd7dec5b234eddbf9b2d7a1231c9a9b
sk2597 <smruti.karade25@gmail.com>
2024-06-09 11:56:40 +0530

    jest helper functions

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -4,6 +4,24 @@ import "@testing-library/jest-dom";
 
 
 describe("test cases for the contact us component", () => {
+
+  beforeAll(() => {
+    console.log("before all, before running all the test cases")
+  })
+
+  beforeEach(() => {
+    console.log("Before each, before each test case")
+  })
+
+
+  afterAll(() => {
+    console.log("afterAll, after running all the test cases");
+  });
+
+  afterEach(() => {
+    console.log("afterEach, after running each test case");
+  });
+
   test("should load contact us component", () => {
     render(<ContactUs />);
     const heading = screen.getByRole("heading");
